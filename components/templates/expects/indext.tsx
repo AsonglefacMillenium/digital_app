3df2a6d6d6a99a4f9ba874bc08d491a52ad7320d
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2024-03-03 23:03:19 +0000

    update expertise sectionb

--- components/templates/expects/indext.tsx
+++ components/templates/expects/indext.tsx
@@ -5,8 +5,12 @@ const ExpertiseSection = () => {
   return (
     <div className="w-full py-20 flex flex-col gap-16 bg-[#001025]">
       <div className="w-[80%] mx-auto flex flex-col gap-4">
-        <Text text="" color="" fontSize={"large"} fontWeight={"semiBold"} />
-        <Text text="" color="#fffff" fontSize={"Xlarge"} fontWeight={"semiBold"} />
+        <Text text="OUR PAST & OUR FUTURE" color="#BF9874" fontSize={"large"} fontWeight={"semiBold"} />
+        <Text text="OUR BEST EXPERTS" color="#fff" fontSize={"XXlarge"} fontWeight={"semiBold"} />
+      </div>
+
+      <div className="w-[80%] margin-auto flex justify-between">
+
       </div>
     </div>
   );
